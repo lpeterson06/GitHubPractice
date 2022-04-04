0b0ce6c3a61f2c8ba1a79371c92ca5ed19b82d36
Logan Peterson <lpeterson4345@stu.d214.org>
2022-04-04 14:20:02 -0500

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Logan Peterson on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
